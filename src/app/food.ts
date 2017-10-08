25f8b2ff49e899622472b8bcd0e89540978195b6
Philip <ich@philipfrank.de>
2017-10-08 09:40:22 +0200

    form fields to track detailed nutrition information

--- src/app/food.ts
+++ src/app/food.ts
@@ -5,4 +5,9 @@ export class Food {
     public picture: string|null,
     public answers: number[],
   ) {}
+
+  public energyValue?: number|null = null;
+  public fat?: number|null = null;
+  public sugar?: number|null = null;
+  public salt?: number|null = null;
 }
